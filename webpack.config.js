defefebfa13171b9bd67482a89231477a8c0ae16
Raphael <rjjaggerd@gmail.com>
2023-10-01 02:40:20 +0300

    Added css & style loaders

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,12 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'), // Allows us to use absolute filepath. __dirname refers to the current directory. From the current direcotry we point to the dist folder for our output
     filename: 'bundle.js', // By default the file name is main.js. This is the file that will be outputted in dis folder
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // RegEx to find any file ending with .css
+        use: ['style-loader', 'css-loader'], //We will apply style-loader and css loader to the test(any file ending with .css)
+      },
+    ],
+  },
 };
