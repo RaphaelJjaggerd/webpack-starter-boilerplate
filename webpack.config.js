77669412a5a0c971db40a4f38d9d5d5a6ebf80b2
Raphael <rjjaggerd@gmail.com>
2023-10-01 03:46:39 +0300

    Added Webpack DevServer plugin

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,16 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'), // Allows us to use absolute filepath. __dirname refers to the current directory. From the current direcotry we point to the dist folder for our output
     filename: 'bundle.js', // By default the file name is main.js. This is the file that will be outputted in dis folder
   },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'), // This is where we want to serve from
+    },
+    port: 3000, // Opens the webpack server on port 3000
+    open: true, // Ensures it opens in the broser immediately when the port is opened.
+    hot: true, // Enables hot reloading
+    compress: true,
+    historyApiFallback: true,
+  },
   module: {
     rules: [
       {
@@ -19,9 +29,9 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Webpack Boilerplate',
-      filename: 'index.html',
-      template: './src/index.html',
+      title: 'Webpack Boilerplate', // The title of the html template
+      filename: 'index.html', // This is the file name of the html that will be created in dist foldr
+      template: './src/index.html', // This is the html file we are actively coding in in our src folder.
     }),
   ],
 };
